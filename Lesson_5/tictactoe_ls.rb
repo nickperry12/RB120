36c87ccb346c1508ce43c6a1a0a55aa660ba0492
Nick Perry <nick.perry12@outlook.com>
2023-05-10 16:20:25 -0700

    final tictactoe update

--- Lesson_5/tictactoe_ls.rb
+++ Lesson_5/tictactoe_ls.rb
@@ -9,15 +9,9 @@ module Formatable
 
   def joinor(array, delimiter = ', ', word = 'or ')
     array.map!(&:to_s)
-  
+
     if array.size > 1
-      index = 0
-      loop do
-        array[index] += delimiter
-        index += 1
-        break if index == array.size - 1
-      end
-      array.insert(-2, word).join
+      array[0..-2].join(delimiter) + ' ' + word + array[-1]
     else
       array[0]
     end
@@ -43,6 +37,8 @@ class Board
     reset
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def draw
     puts ""
     puts "     |     |"
@@ -58,8 +54,10 @@ class Board
     puts "     |     |"
     puts ""
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 
-  def set_square_at(key, marker)
+  def []=(key, marker)
     @squares[key].marker = marker
   end
 
@@ -80,7 +78,7 @@ class Board
       possible_win_line = @squares.values_at(*line)
       possible_win_line.map!(&:marker)
       return TTTGame::HUMAN_MARKER if possible_win_line.count("X") == 3
-      return TTTGame::COMPUTER_MARKER if possible_win_line.count("O") == 3
+      return TTTGame::CPU_MARKER if possible_win_line.count("O") == 3
     end
     nil
   end
@@ -145,11 +143,8 @@ class Human < Player
     prompt MSG['enter_name']
     loop do
       name = gets.chomp
-      if name.gsub(' ', '').nil? || name.gsub(' ', '').empty?
-        prompt MSG['invalid_name']
-      else
-        break
-      end
+      break unless name.gsub(' ', '').nil? || name.gsub(' ', '').empty?
+      prompt MSG['invalid_name']
     end
     self.name = name
   end
@@ -157,8 +152,8 @@ end
 
 class Computer < Player
   def set_name
-    names = ["R2D2", "BD-1", "C3PO", "HAL 9000", "Bender", "RoboCop", "Johnny 5"]
-    self.name = names.sample
+    list = ["R2D2", "BD-1", "C3PO", "HAL 9000", "Bender", "RoboCop", "Johnny 5"]
+    self.name = list.sample
   end
 
   def detect_threat(board)
@@ -167,19 +162,19 @@ class Computer < Player
         board.values_at(*line).map(&:marker).count(' ') == 1
     end.flatten
   end
-  
+
   def detect_win(board)
     Board::WINNING_LINES.select do |line|
-      board.values_at(*line).map(&:marker).count(TTTGame::COMPUTER_MARKER) == 2 &&
+      board.values_at(*line).map(&:marker).count(TTTGame::CPU_MARKER) == 2 &&
         board.values_at(*line).map(&:marker).count(' ') == 1
     end.flatten
   end
 
-  def attack_threat_square(line, board)
+  def get_threat_square(line, board)
     line.select { |square| board[square].marker == Square::INITIAL_MARKER }[0]
   end
 
-  def attack_win_square(line, board)
+  def get_win_square(line, board)
     line.select { |square| board[square].marker == Square::INITIAL_MARKER }[0]
   end
 end
@@ -188,15 +183,25 @@ class TTTGame
   include Formatable
 
   HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
+  CPU_MARKER = "O"
 
-  attr_reader :board, :human, :computer
+  attr_reader :board, :human, :cpu, :current_player
 
   def initialize
     @board = Board.new
     @human = Human.new(HUMAN_MARKER)
-    @computer = Computer.new(COMPUTER_MARKER)
+    @cpu = Computer.new(CPU_MARKER)
+    @current_player = [HUMAN_MARKER, CPU_MARKER].sample
   end
+
+  def play
+    game_setup
+    game_loading
+    main_game_loop
+    display_goodbye_message
+  end
+
+  private
 
   def clear_screen
     system "clear"
@@ -221,11 +226,48 @@ class TTTGame
   end
 
   def display_board
-    prompt "#{human.name}'s marker is an X. #{computer.name}'s marker is a #{computer.marker}."
+    prompt "#{human.name}'s marker is an X."
+    prompt "#{cpu.name}'s marker is a #{cpu.marker}."
     puts ""
     display_score
     puts ""
     board.draw
+  end
+
+  def display_result
+    clear_screen_display_board
+    case board.winning_marker
+    when human.marker
+      prompt MSG['player_wins']
+    when cpu.marker
+      prompt MSG['cpu_wins']
+    else
+      prompt MSG['tie']
+    end
+  end
+
+  def display_score
+    prompt MSG['current_score']
+    puts ""
+    prompt "#{human.name}: #{human.wins} wins, #{human.losses} losses."
+    prompt "#{cpu.name}: #{cpu.wins} wins, #{cpu.losses} losses."
+  end
+
+  def confirm_play_again_message
+    prompt MSG['another_one']
+    state_first_player
+    prompt MSG['loading']
+    sleep(5)
+  end
+
+  def state_first_player
+    puts ""
+    prompt MSG['first_move'] + human.name if human_turn?
+    prompt MSG['first_move'] + cpu.name if cpu_turn?
+  end
+
+  def human_turn?
+    current_player == HUMAN_MARKER
   end
 
   def human_moves
@@ -238,33 +280,71 @@ class TTTGame
       prompt MSG['invalid_choice']
     end
 
-    board.set_square_at(square, human.marker)
+    board.[]=(square, human.marker)
   end
 
-  def computer_moves(brd) 
-    threat_line = computer.detect_threat(brd.squares)
-    win_line = computer.detect_win(brd.squares)
-    # binding.pry
-    if threat_line.empty? && win_line.empty? && brd.squares[5].marker == ' '
-      brd.set_square_at(5, computer.marker)
-    elsif !win_line.empty?
-      brd.set_square_at(computer.attack_win_square(win_line, brd.squares), computer.marker)
-    elsif !threat_line.empty?
-      brd.set_square_at(computer.attack_threat_square(threat_line, brd.squares), computer.marker)
+  def cpu_turn?
+    current_player == CPU_MARKER
+  end
+
+  def cpu_randomly_picks_square
+    board[board.unmarked_keys.sample] = cpu.marker
+  end
+
+  def cpu_place_piece_at_5
+    board[5] = cpu.marker
+  end
+
+  def cpu_mark_win_square(win_line)
+    board[cpu.get_win_square(win_line, board.squares)] = cpu.marker
+  end
+
+  def cpu_mark_threat_square(threat_line)
+    board[cpu.get_threat_square(threat_line, board.squares)] = cpu.marker
+  end
+
+  # The following 3 methods are conditional checks to be used in the
+  # `possible_cpu_moves` method. These were extracted into their own methods in
+  # an attempt to appeas the `rubocop AbcSize` cop. I think it's a bit redundant
+  # but did so anyway
+
+  def check_for_square_5(brd, threat_line, win_line)
+    threat_line.empty? && win_line.empty? && brd.squares[5].marker == ' '
+  end
+
+  def check_for_possible_win(win_line)
+    !win_line.empty?
+  end
+
+  def check_for_any_threat(threat_line)
+    !threat_line.empty?
+  end
+
+  def possible_cpu_moves(threat_line, win_line)
+    if check_for_square_5(board, threat_line, win_line)
+      cpu_place_piece_at_5
+    elsif check_for_possible_win(win_line)
+      cpu_mark_win_square(win_line)
+    elsif check_for_any_threat(threat_line)
+      cpu_mark_threat_square(threat_line)
     else
-      brd.set_square_at(brd.unmarked_keys.sample, computer.marker)
+      cpu_randomly_picks_square
     end
   end
 
-  def display_result
-    clear_screen_display_board
-    case board.winning_marker
-    when human.marker
-      prompt MSG['player_wins']
-    when computer.marker
-      prompt MSG['cpu_wins']
-    else
-      prompt MSG['tie']
+  def cpu_moves(brd)
+    threat_line = cpu.detect_threat(brd.squares)
+    win_line = cpu.detect_win(brd.squares)
+    possible_cpu_moves(threat_line, win_line)
+  end
+
+  def current_player_moves
+    if cpu_turn?
+      cpu_moves(board)
+      @current_player = HUMAN_MARKER
+    elsif human_turn?
+      human_moves
+      @current_player = CPU_MARKER
     end
   end
 
@@ -272,18 +352,11 @@ class TTTGame
     case board.winning_marker
     when HUMAN_MARKER
       human.increment_wins
-      computer.increment_losses
-    when COMPUTER_MARKER
-      computer.increment_wins
+      cpu.increment_losses
+    when CPU_MARKER
+      cpu.increment_wins
       human.increment_losses
     end
-  end
-
-  def display_score
-    prompt MSG['current_score']
-    puts ""
-    prompt "#{human.name}: #{human.wins} wins, #{human.losses} losses."
-    prompt "#{computer.name}: #{computer.wins} wins, #{computer.losses} losses."
   end
 
   def play_again?
@@ -297,51 +370,46 @@ class TTTGame
     end
 
     return true if ["y", "yes"].include?(choice)
-    return false if ["n", "no"].include?(choice)
-  end
-
-  def main_game_loop
-    in_game = true
-
-    until in_game == false
-      display_board
-
-      loop do
-        human_moves
-        break if board.someone_won? || board.full?
-        computer_moves(board)
-        break if board.someone_won? || board.full?
-        clear_screen_display_board
-      end
-      update_score
-      display_result
-      in_game = false unless play_again?
-      reset unless in_game == false
-    end
+    false
   end
 
   def game_loading
-    prompt "Please wait a moment while the game loads..."
+    prompt MSG['loading']
     sleep(5)
     system 'clear'
   end
 
   def game_setup
+    clear_screen
     display_welcome_message
     human.set_name
-    computer.set_name
-  end
-
-  def play
-    game_setup
-    game_loading
-    main_game_loop
-    display_goodbye_message
+    cpu.set_name
+    state_first_player
   end
 
   def reset
     board.reset
     clear_screen
+  end
+
+  def player_turn_loop
+    loop do
+      current_player_moves
+      break if board.someone_won? || board.full?
+      clear_screen_display_board
+    end
+  end
+
+  def main_game_loop
+    loop do
+      display_board
+      player_turn_loop
+      update_score
+      display_result
+      break unless play_again?
+      confirm_play_again_message
+      reset
+    end
   end
 end
 
