afef465ef1bb9cfc7a0fed061494007f16795ffa
nickperry <nickperry604@gmail.com>
2023-05-28 12:23:33 -0700

    file updates

--- Nicks_Laboratory/code_tests.rb
+++ Nicks_Laboratory/code_tests.rb
@@ -1,27 +1,13 @@
-class Person
-  attr_reader :hand
-
-  def initialize
-    @hand = []
+class Dog
+  def walk
+    "walking"
   end
 end
 
-class Dealer
-  attr_reader :human
-
-  def initialize
-    @human = Person.new
-  end
-
-  def deal_cards
-    human.hand << [10, 'Hearts']
-  end
-
-  def show_cards
-    human.hand
+class Human
+  def walk
+    "walking"
   end
 end
 
-dealer = Dealer.new
-dealer.deal_cards
-p dealer.show_cards
+[Human.new, Dog.new].each { |obj| puts obj.walk } 
